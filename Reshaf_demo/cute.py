90044cc4f529597736aba7cb47937423026f94b1
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-04-22 11:14:11 +0530

    MY DAY THIRD OF LEARN PYTHON

--- Reshaf_demo/cute.py
+++ Reshaf_demo/cute.py
@@ -1,34 +1,95 @@
-# # # # # count = 1
-# # # # # while count < 5 :
-# # # # #     print("Yasmin cutie")
-# # # # #     count += 1
+# nums = (2, 4, 76, 8, 9, 0, 34, 14, 87, 46, 88, 99, )
+# x = 34
+# i = 0
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("number found", i)
+#         continue
+#     else:
+#         print("number not found", i)
+#     i += 1
+#     break
 
 
-# # # # n = int(input("enter a number for table = "))
-# # # # i = 1
-# # # # while i <= 10 :
-# # # #     print(n * i)
-# # # #     i += 1
+
+# nums = [3, 4,8 ,5, 9, 0, ]
+# for name in nums:
+#     print(name)
+
+# str = "Zaynul Abdeen"
+
+# for char in str :
+#     if(char == "l"):
+#         print(" l found")
+#         break
+#     print(char)
+# else:
+#     print("end")
+ 
+
+# n = [1, 4, 3, 6, 9, 4, 0, 34, 200, ]
+
+# for x in n :
+#     print(x)
 
 
-# # nums = ( 2, 12, 4, 3, 92, 99, 45, 5, 8, 20, 4, )
-# # idx = 0
-# # while idx < len(nums) :
-# #     print(nums(idx))
-# #     idx += 1
+# tup =  (4, 7, 9, 2, 0, 5, 8, 33, 55, 7,)
+# x = 33
 
-nums = ( 2, 12, 4, 3, 92, 99, 45, 5, 8, 20, 4, 36, )
-x = 99
-i = 0 
-found = False
+# idx = 0
+# for el in tup:
+#     if(el == x):
+#         print("num found", idx)
+#         break
+#     idx += 1
+
+# else
+#     print("num not found")
 
 
-while i < len(nums):
-    if nums[i] == x :
-     print("found", i)
-     found = True
-     break
-    i += 1
-if not found:
-   print("not found")    
-    
+# el = range(10000)
+
+# for i in el:
+#     print(i)
+
+# for el in range(300):
+#     print(el)
+ 
+
+# for el in range(9,200):
+#     print(el)
+
+# for el in range(2,22299,1000):
+#     print(el)
+
+
+# Practice Question
+# for el in range(100,0,-1):
+#     print(el)
+
+#Pass Statement
+# for el in range(20):
+#     pass
+# print("Zaynul Abdeen")
+
+#Find the the sum of fisrt natural number
+# n = 7
+# sum = 0
+# for el in range(1, n+1):
+#     sum += el
+#     print("sum of num=",sum)
+
+# n = 7
+# i = 1 
+# sum = 0
+# while i <= n:
+#     sum += i
+#     i += 1
+#     print("sum of num=",sum)
+
+#WAP to find  the factorial of first natural num .
+n = 5
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+    print(fact)
