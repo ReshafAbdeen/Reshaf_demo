293daf779583a91f4b446472534cf2b4f03ad250
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-04-22 11:17:45 +0530

    MY DAY THIRD OF LEARNING PYTHON

--- Reshaf_demo/cute.py
+++ Reshaf_demo/cute.py
@@ -1,91 +1,100 @@
-# nums = (2, 4, 76, 8, 9, 0, 34, 14, 87, 46, 88, 99, )
-# x = 34
-# i = 0
-# while i < len(nums):
-#     if(nums[i] == x):
-#         print("number found", i)
-#         continue
-#     else:
-#         print("number not found", i)
-#     i += 1
-#     break
+#MY DAY THIRD OF LEARN PYTHON
+
+
+#Practice
+nums = (2, 4, 76, 8, 9, 0, 34, 14, 87, 46, 88, 99, )
+x = 34
+i = 0
+while i < len(nums):
+    if(nums[i] == x):
+        print("number found", i)
+        continue
+    else:
+        print("number not found", i)
+    i += 1
+    break
+
+
+#Practice
+nums = [3, 4,8 ,5, 9, 0, ]
+for name in nums:
+    print(name)
 
 
 
-# nums = [3, 4,8 ,5, 9, 0, ]
-# for name in nums:
-#     print(name)
+#for loops with else
+str = "Zaynul Abdeen"
 
-# str = "Zaynul Abdeen"
+for char in str :
+    if(char == "l"):
+        print(" l found")
+        break
+    print(char)
+else:
+    print("end")
+ 
+#Print the element of the following liist using loop
+n = [1, 4, 3, 6, 9, 4, 0, 34, 200, ]
 
-# for char in str :
-#     if(char == "l"):
-#         print(" l found")
-#         break
-#     print(char)
-# else:
-#     print("end")
+for x in n :
+    print(x)
+
+#Search for a numm x in this touple using loop
+tup =  (4, 7, 9, 2, 0, 5, 8, 33, 55, 7,)
+x = 33
+
+idx = 0
+for el in tup:
+    if(el == x):
+        print("num found", idx)
+        break
+    idx += 1
+
+else:
+    print("num not found")
+
+# range()
+el = range(10000)
+
+for i in el:
+    print(i)
+
+for el in range(300):
+    print(el)
  
 
-# n = [1, 4, 3, 6, 9, 4, 0, 34, 200, ]
+for el in range(9,200):
+    print(el)
 
-# for x in n :
-#     print(x)
-
-
-# tup =  (4, 7, 9, 2, 0, 5, 8, 33, 55, 7,)
-# x = 33
-
-# idx = 0
-# for el in tup:
-#     if(el == x):
-#         print("num found", idx)
-#         break
-#     idx += 1
-
-# else
-#     print("num not found")
-
-
-# el = range(10000)
-
-# for i in el:
-#     print(i)
-
-# for el in range(300):
-#     print(el)
- 
-
-# for el in range(9,200):
-#     print(el)
-
-# for el in range(2,22299,1000):
-#     print(el)
+for el in range(2,22299,1000):
+    print(el)
 
 
 # Practice Question
-# for el in range(100,0,-1):
-#     print(el)
+for el in range(100,0,-1):
+    print(el)
 
-#Pass Statement
-# for el in range(20):
-#     pass
-# print("Zaynul Abdeen")
+
+for el in range(20):
+    pass
+print("Zaynul Abdeen")
 
 #Find the the sum of fisrt natural number
-# n = 7
-# sum = 0
-# for el in range(1, n+1):
-#     sum += el
-#     print("sum of num=",sum)
+n = 7
+sum = 0
+for el in range(1, n+1):
+    sum += el
+    print("sum of num=",sum)
 
-# n = 7
-# i = 1 
-# sum = 0
-# while i <= n:
-#     sum += i
-#     i += 1
-#     print("sum of num=",sum)
+n = 7
+i = 1 
+sum = 0
+while i <= n:
+    sum += i
+    i += 1
+    print("sum of num=",sum)
+
+#Pass 
 
 #WAP to find  the factorial of first natural num .
 n = 5
