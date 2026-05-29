2fc49bbd83412f606a70f402ea0efc08be58f672
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-05-29 16:04:41 +0530

    Hollywood Movie Insights & Explorer

--- cutie.py
+++ cutie.py
@@ -1,24 +1,34 @@
-class AImodel:
-    def __init__(self, model_name, accuracy):
-        self.name = model_name
-        self.accuracy = accuracy
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-    def show_detail(self):
-        print(f"Model ka Name {self.name}")
-        print(f"Model ki accuracy {self.accuracy}")
+url = 'https://raw.githubusercontent.com/wtitze/3E4A28/master/IMDb-movies.csv'
 
-    def predict(self, data):
-        print(f"{self.name} is predicting result for {data}")
+print("\nMovie Dataset load ho raha hai, thoda sa intezar karein...")
+columns_needed = ['title', 'year', 'genre', 'duration', 'votes', 'budget', 'usa_gross_income']
+df = pd.read_csv(url, usecols=columns_needed)
 
+print("Data successfully load ho gaya!")
+print(f"Total Movies ka Data: {df.shape[0]} Rows, {df.shape[1]} Columns\n")
 
-model1 = AImodel("Zomato_Rzter", 93.4)
-model2 = AImodel("Desi_chatgpt", 94.3)
+print("--- Cleaning Missing Data ---")
+df['budget'] = df['budget'].fillna(0)
+df['usa_gross_income'] = df['usa_gross_income'].fillna(0)
+print("✔ Missing values ko clean kar diya gaya hai.\n")
 
-print("\n---Model1 Testing---")
-model1.show_detail()
-model1.predict("Zomato resturant ka data")
+top_movies = df.nlargest(5, 'votes')
 
+print("--- Top 5 Most Popular Movies ---")
+print(top_movies[['title', 'year', 'genre', 'votes']])
 
-print("\n---Model2 Testing---")
-model2.show_detail()
-model2.predict("Bhai python oops kiya hota hai")
+print("\nGraph taiyar ho raha hai...")
+plt.figure(figsize=(10, 6))
+
+sns.barplot(x='votes', y='title', data=top_movies, palette='coolwarm')
+
+plt.title("IMDB Top 5 Most Voted Movies", fontsize=16, fontweight='bold')
+plt.xlabel("Total Votes (Popularity)", fontsize=12)
+plt.ylabel("Movie Name", fontsize=12)
+
+print(" Graph khulne wala hai...")
+plt.show()
