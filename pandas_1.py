afb7db978b8ccfa2ab330873cd3c83130d46d919
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-06-02 09:58:08 +0530

    Pandas Series

--- pandas_1.py
+++ pandas_1.py
@@ -1,125 +1,123 @@
-#Day 1 of pandas
-
-import pandas as  pd 
+#Pandas series
+import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-url = 'https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv'
-
-print("\nInternet se data magay hai....")
-df = pd.read_csv(url)
-
-
-df.to_csv('california_housing', index=False)
-print("File save as 'calipornia_housing.csv'!\n")
-
-print("\ndata load ho gya hai..")
-
-print("Total ghar faeture(Row, Columns):")
-print(df.shape)
-
-print("\n--Top 3 House--")
-print(df.head(3))
-
-print("Gharo ka data....")
-df['total_bedrooms'] = df["total_bedrooms"].fillna(0)
-print(df.isnull().sum())
-print(df.info())
-
-
-plt.figure(figsize = (9, 7))
-sns.barplot(x = 'ocean_proximity', y = 'median_house_value', data = df, palette = 'viridis')
-
-plt.title("California House Prices vs Ocean Proximity", fontsize=16, fontweight='bold')
-plt.xlabel("Samandar Se Doori (Ocean Proximity)", fontsize=12)
-plt.ylabel("Ghar Ki Keemat (Median House Value in USD)", fontsize=12)
-plt.grid(True, linestyle='--', alpha=0.1)
-
-plt.show()
-
-
-
-
-#Day 2 of pandas
-
-import pandas as pd 
-
-class DataArregator:
-    def __init__(self, file_url):
-        print("\nData load ho rha hai....")
-        self.df = pd.read_csv(file_url)
-
-    def area_wise_avg(self):
-        print("\n--Average House Value by Ocean Proximity--")
-        summary = self.df.groupby('ocean_proximity')['median_house_value'].mean().reset_index()
-        print(summary)
-
-    def grt_area_wise_stats(self):
-        print("\n--Population (Total) & House Value (Max) by Area--")
-        advaced_summary = self.df.groupby('ocean_proximity').agg({
-            'median_house_value' : 'max',
-            'population' : 'sum'
-              })
-        print(advaced_summary)
-
-if __name__ == "__main__":
-         url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv"
-
-agg_bot = DataArregator(url)
-agg_bot.area_wise_avg()
-agg_bot.grt_area_wise_stats()
-
-
-
-#Sorted Aggregation Class
-
-import pandas as pd 
-
-class SortedDataAnalyzer:
-    def __init__(self, file_url):
-        print("\n--Data load ho rha hai--")
-        self.df = pd.read_csv(file_url)
-        print("\n--Data ;oad ho gya hai--")
-
-    def get_top_expensive_areas(self):
-        print("\n--Area Ranked by Average House Value (Highest to Lowest)--")
-        summary = self.df.groupby('ocean_proximity')['median_house_value'].mean().reset_index()
-        summary = summary.rename(columns = {'median_house_value' : 'Avg_value'})
-        sorted_summary = summary.sort_values(by = 'Avg_value', ascending=False)
-        print(sorted_summary)
-if __name__ == "__main__":
-    url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv"
-rank_bot = SortedDataAnalyzer(url)
-rank_bot.get_top_expensive_areas()
-
-
-
-import pandas as pd
-
-class DataCleaner:
+class SeriesMaster:
     def __init__(self, file_url):
         print("--Data load ho rha hai--")
         self.df = pd.read_csv(file_url)
-        print("--Data load ho gya hai--")
+        print ("--Data loa ho gya hai--")
+
+    def find_ocean_trend(self):
+        print("Counting House eanch areas (Trend)")
+        area_count = self.df['ocean_proximity'].value_counts()
+        print(area_count)
+
+
+    def lowercase_ocean_text(self):
+        print("Converting Ocean proximity to lowercase using apply method...")
+        def make_lowercase(text):
+            return text.lower()
+        
+        self.df['ocean_proximity_clean'] = self.df['ocean_proximity'].apply(make_lowercase)
     
-    def check_missing_data(self):
-        print("\n--Missing value in each column--")
-        missing_val_count = self.df.isnull().sum()
-        print(missing_val_count)
+    def plot_ocean_data(self):
+        print("Bar plot chart ban ke ready ho rha hia ...")
+        area_count = self.df['ocean_proximity_clean'].value_counts().reset_index()
 
-    def fill_missing_val(self):
-        print("\n--Fixing Missing Values--")
-        bedrooms_median = self.df['total_bedrooms'].median()
-        print(f"Total bedroom ka median : {bedrooms_median}")
-        self.df['total_bedrooms'] = self.df['total_bedrooms'].fillna(bedrooms_median)
+        plt.figure(figsize = (7,8))
+        sns.barplot(x ='ocean_proximity_clean', y = 'count', palette = 'viridis', data = area_count )
+        plt.title('Ocean ke  kinare ke ghar', fontsize = 14)
+        plt.xlabel('House name', fontsize = 22)
+        plt.ylabel('Ghar ki keemte', fontsize = 22)
+        plt.grid(True, linestyle='--', alpha=0.5)
+        plt.show()
 
-        print("\n--Missing value ko succesfully fill kar diya gya hai--")
-        print("\n--Re-checking Missing values to confirm--")
-        print(self.df.isnull().sum())
 
 if __name__ == "__main__":
     url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv"
 
-clear_bot = DataCleaner(url)
-clear_bot.check_missing_data()
-clear_bot.fill_missing_val()
+Master_bot = SeriesMaster(url)
+Master_bot.find_ocean_trend()
+Master_bot.lowercase_ocean_text()
+Master_bot.plot_ocean_data()
+  
+
+
+#Advanced Methods Ki Nayi Class
+import pandas as pd
+class AdvanceMethod:
+    def __init__(self, file_url):
+        print("--Data load ho rah hai--")
+        self.df = pd.read_csv(file_url)
+        print("data load ho gya hai''''")
+
+
+    def get_data_summary(self):
+        print('\n-----Statistics summary of Dataset----- ')
+        print(self.df[['houing_median_age', 'median_house_value']].describe())
+
+    def filter_with_isin(self):
+        print("\n---Only targeted areas Island and Near Bay-----")
+        area_target = ['ISLAND', 'NEAR BAY']
+        targetd_df = self.df[self.df['ocean_proximity'].isin(area_target)]
+        print(f"Match hone wale kul gharo ki sankhiya : {len(targetd_df)}")
+        print(targetd_df[['ocean_proximity', 'median_house_value']].head(3))
+
+
+    def filter_with_querry(self):
+        print("-----Filtering using querry (Age > 50 & Price < 150000)-----")
+        query_df = self.df.query('housing_median_age > 50 and median_house_value < 150000')
+        print(f"Puran or saste gharo ki ginti : {len(query_df)}")
+        print(query_df[['housing_median_age', 'median_house_value']].head(3))
+
+if __name__ == "__main__":
+    url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv"
+
+advanced_bot = AdvanceMethod(url)
+advanced_bot.filter_with_querry()
+advanced_bot.filter_with_isin()
+advanced_bot.filter_with_querry()
+
+
+#Advanced Methods Part-B Ki Nayi Class
+import pandas as pd 
+
+class PandasTransformer:
+    def __init__(self, file_url):
+        print("---Data loa ho rha hai---")
+        self.df = pd.read_csv(file_url)
+        print("---Data load ho gaya hai---")
+
+    def drop_faulty_columns(self):
+        print("---Faltu columns ko draop kar dete hai to easy to read---")
+        cleaned_of = self.df.drop(columns=['longitude', 'latitude'])
+
+        print(f"Pegle ka ganda data : {len(self.df.columns)}")
+        print(f"Saaf hone ke Baad ka data : {len(cleaned_of.columns)}")
+        print("Bache hue columns :", list(cleaned_of.columns))
+
+
+    def rename_mycolumns(self):
+        print("---Naam badlne ke baad ka data kaisa dikhta hai---")
+        rename_df = self.df.rename(columns = {
+            'housing_median_age': 'house_age',
+            'median_house_value' : 'price'
+        })
+
+        print(rename_df[['house_age', 'price']].head(3))
+
+
+    def get_randome_sample(self):
+        randome_rows = self.df.sample(n=3)
+        print(randome_rows[['ocean_proximity','housing_median_age' ]])
+
+if __name__ == "__main__":
+    url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv"
+
+transformer_bot = PandasTransformer(url)
+transformer_bot.drop_faulty_columns()
+transformer_bot.rename_mycolumns()
+transformer_bot.get_randome_sample()
+
