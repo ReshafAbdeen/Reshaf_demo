4d3eef5946844698e429c149f59f410d31ea6e19
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-06-05 22:48:44 +0530

    Diamond Pettern

--- cutie.py
+++ cutie.py
@@ -1,3 +1,17 @@
-a = 5
-for i in range(1, a + 1):
-       print(" "* (a -i) + "*"* i)
+#Diamond Pettern
+n = 5
+for i in range(1, n + 1):
+    for j in range(1, 2 * n):
+        if j == n - i + 1 or j == n + i - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
+for i in range(n - 1, 0, -1):
+    for j in range(1, 2 * n):
+        if j == n - i + 1 or j == n + i - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
