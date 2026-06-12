a3513d9837f59b7c79ee95ada0227e394dfd5376
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-06-12 09:58:51 +0530

    The Mind Reader Game

--- cute.py
+++ cute.py
@@ -1,17 +1,39 @@
-#The Tech Interview Puzzel
+#The Mind Reader Game
 
-print("\033[1m"+"Welcome to The Tech Interview Puzzel"+"\033[0m")
+import random
+print("\033[1m" + "=== Welcome to the Mind Reader Game ===" + "\033[0m\n")
+secret_number = random.randint(1, 20)
+print("Maine 1 se 20 ke beech ek number socha hai Guess karo konsa hai ???")
+lifeline = 7
+sahiGuess = False
 
-user = int(input("Enter a random number : "))
-for i in range(1, user + 1 ):
-    if i % 3== 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    elif  i % 3 == 0 and i % 5 == 0:
-        print("FizzBuss")
-            
-    else:
-        print(i)
-          
+while lifeline > 0 and not sahiGuess:
+    print()
+    print(f"Aapke ke pass {lifeline} moke hai.")
+    user = int(input("Enter Your Guess number : "))
+    
+    
+
+    if user == secret_number:
+        print("Wahhh1 Bhai sahi tum to genius ho...")
+        sahiGuess = True
         
+    elif user < secret_number:
+        print("Bhai thoda bara number guess karo..")
+        lifeline -= 1
+
+    elif user < 1 or user > 20 :
+        print("Warning !!!!  Bhai 1 se 20 ke beech hi number guess karo.. ")
+        lifeline -= 1
+
+    else:
+        print("THoda chota number guess karo..")
+        lifeline -= 1
+
+
+if sahiGuess:
+    print("\n\033[1;36m" + "Aap Jeet Gaye! Kya Mind Reading ki hai!" + "\033[0m")
+else:
+    print("\n\033[1;31m" + f"Lifelines Khatam! Game Over bhaiya. Sahi number {secret_number} tha!" + "\033[0m")
+
+print("\033[1m" + "=== Game Over !!! ===" + "\033[0m\n")
