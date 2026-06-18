413d8aeaf0c4edd56c48a63049658423629c5b0a
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-06-18 22:54:31 +0530

    Practice

--- day-32.py
+++ day-32.py
@@ -1,13 +1,17 @@
-def my_decorator(func):
-    def wrapper():
-        print("Something is happening BEFORE the function is called.")
-        func()
-        print("Something is happening AFTER the function is called.")
-    return wrapper
 
-@my_decorator
-def say_hello():
-    print("Hello World!")
 
-say_hello()
+employees = [
+    {"name": "Alice", "role": "developer", "salary": 90000},
+    {"name": "Bob", "role": "designer", "salary": 75000},
+    {"name": "Charlie", "role": "developer", "salary": 110000},
+]
 
+devs_with_bonus = [
+    {**emp, "bonus": emp["salary"] * 0.10}
+    for emp in employees
+    for emp in employees 
+    if emp["role"] == "developer"
+]
+
+# 3. Clean Output
+print(*(f"{e['name']}: ${e['bonus']:.0f} Bonus" for e in devs_with_bonus), sep="\n")
