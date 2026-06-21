25ee46ec525e7ef14e90b4e7a4b916e48e535a99
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-06-21 22:34:56 +0530

    Only Practice

--- day-32.py
+++ day-32.py
@@ -1,27 +1,16 @@
-import time
-from contextlib import contextmanager
+def infinite_sequence(start=0):
+    num = start
+    while True:
+        yield num
+        num += 1
 
+gen = infinite_sequence()
 
-@contextmanager
-def execution_timer(block_name: str):
-    """Timer that automatically measures the execution of a 'with' block."""
-    print(f"⏱️ [Entering] Starting timer for: '{block_name}'")
-    start_time = time.perf_counter()
+print(next(gen))  
+print(next(gen))  
+print(next(gen))  
 
-    try:
-        yield
-    finally:
-        end_time = time.perf_counter()
-        print(
-            f"[Exiting] '{block_name}' took {end_time - start_time:.4f} seconds.\n"
-        )
-
-
-if __name__ == "__main__":
-    with execution_timer("Heavy Calculation"):
-        print("Processing data...")
-        _ = [x**2 for x in range(5_000_000)]
-
-    with execution_timer("Network Delay Simulation"):
-        print("Simulating a slow network request...")
-        time.sleep(0.5)
+for val in infinite_sequence(10):
+    print(val)
+    if val >= 13:
+        break  
