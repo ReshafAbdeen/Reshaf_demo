bffb1f2bda284485d38e23959729937d7fa317d0
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-06-22 22:49:22 +0530

    Pracitec

--- day-32.py
+++ day-32.py
@@ -1,16 +1,33 @@
-def infinite_sequence(start=0):
-    num = start
-    while True:
-        yield num
-        num += 1
+import requests
+from bs4 import BeautifulSoup
 
-gen = infinite_sequence()
 
-print(next(gen))  
-print(next(gen))  
-print(next(gen))  
+class QuoteScraper:
 
-for val in infinite_sequence(10):
-    print(val)
-    if val >= 13:
-        break  
+    def __init__(self):
+        self.url = "https://quotes.toscrape.com/"
+
+    def fetch_quotes(self):
+        response = requests.get(self.url)
+        if response.status_code != 200:
+            return "Failed to retrieve website."
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        quote_elements = soup.find_all("div", class_="quote")
+
+        scraped_data = []
+        for element in quote_elements:
+            text = element.find("span", class_="text").text
+            author = element.find("small", class_="author").text
+            scraped_data.append({"quote": text, "author": author})
+
+        return scraped_data
+
+
+if __name__ == "__main__":
+    scraper = QuoteScraper()
+    results = scraper.fetch_quotes()
+
+    print(f"--- Scraped {len(results)} Quotes ---")
+    for item in results[:5]:
+        print(f"\"{item['quote']}\" - By: {item['author']}\n")
