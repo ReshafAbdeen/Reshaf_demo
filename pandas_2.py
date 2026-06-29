95108dbe15280dc4ba386e702d41438c1e19ef2b
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-06-29 09:42:16 +0530

    Pandas Pracice

--- pandas_2.py
+++ pandas_2.py
@@ -1,33 +1,25 @@
-import pandas as pd
+import pandas as pd 
 import numpy as np
 
-store_data = {
-    "Item": ["ASUS TUF Laptop", "Logitech Mouse", "Ryzen 5 CPU", "ASUS TUF Laptop", "Logitech Mouse", "Mechanical Keyboard", "Ryzen 5 CPU"],
-    "Category": ["Laptop", "Accessories", "Components", "Laptop", "Accessories", "Accessories", "Components"],
-    "Quantity_Sold": [3, 10, 2, 1, 15, 5, None],  
-    "Price_Per_Item": [75000, 15000, 18000, 75000, 1200, 4500, 18000],
-    "Customer_Rating": [4.5, 4.2, 4.8, 4.6, None, 4.0, 4.7]
+print("\n===Numpy + Pandas Power combo===")
+data = {
+    "student" : ['Zaynul', 'Rahul', 'Amit', 'Sana', 'Vikram'],
+    "Python_score" : [95, 89, 23, 4, 34],
+    "Attendence": [90, 58, 29, 34, 85]
 }
 
-df = pd.DataFrame(store_data)
-# print(df)
-
-df["Quantity_Sold"] = df["Quantity_Sold"].fillna(0)
-
-avg_rarting = df["Customer_Rating"].mean()
-df["Customer_Rating"] = df["Customer_Rating"].fillna(avg_rarting)
-
-df["Total_Revenue"] = df["Quantity_Sold"] * df["Price_Per_Item"]
-
-category_sale = df.groupby("Category")["Total_Revenue"].sum()
-
-print("\n----Cleaned data after processing----")
+df = pd.DataFrame(data)
+print("\n==Asli Data==")
 print(df)
 
+df['Status'] = np.where(df['Python_score'] >= 75, 'Pass', 'Fail')
+# condition = 
+print("\n==Status ke baad ka Data")
+print(df)
 
-print("\n----Sale by Category----")
-print(category_sale)
+condition = (df['Python_score'] > 80) & (df['Attendence'] > 80)
+df['Category'] = np.where(condition, 'scholar', 'standard')
 
-
-best_item = df.loc[df["Quantity_Sold"].idxmax()]['Item']
-print(f"\n----{best_item} Sabse mehga item----")
+df['Detained'] = np.where(df['Attendence'] >= 75, 'No', 'Yes')
+print("\n==Final AI filtered Data==")
+print(df)
