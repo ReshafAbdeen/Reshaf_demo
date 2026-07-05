e12a9d105d9dcc23cee9d08438d4f7b405115209
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-07-05 22:25:56 +0530

    Pandas Practice

--- cute.py
+++ cute.py
@@ -1,26 +1,17 @@
-#Crypto Trend Analyzer
-
 import pandas as pd
 
-print("\033[1m" + "=== Crypto Time Series Analyzer ===" + "\033[0m\n")
-
-market_data = {
-    "Date": ["2026-07-01", "2026-07-02", "2026-07-03", "2026-07-04", "2026-07-05", "2026-07-06"],
-    "Coin": ["Bitcoin", "Bitcoin", "Bitcoin", "Ethereum", "Ethereum", "Ethereum"],
-    "Price_USD": [60500, 62000, 59000, 3100, 3250, 3050]
+# Create a sample dataset of employees
+data = {
+    'Name': ['Amit', 'Priya', 'Rahul', 'Neha', 'Vikram', 'Sneha'],
+    'Department': ['IT', 'HR', 'IT', 'Finance', 'Finance', 'HR'],
+    'Salary': [65000, 52000, 72000, 85000, 48000, 56000],
+    'Experience_Yrs': [3, 2, 5, 8, 1, 4]
 }
 
-df = pd.DataFrame(market_data)
-print("--- Raw Data (Date is Text) ---")
-print(df)
-print("\n---Datatype of Data Column :" , type(df['Date'][0]))
+df = pd.DataFrame(data)
 
+experienced = df[df['Experience_Yrs'] > 2]
 
-df['Date'] = pd.to_datetime(df['Date'])
+avg_salary_dept = experienced.groupby('Department')['Salary'].mean()
 
-df['Day'] = df['Date'].dt.day
-df['Month'] = df['Date'].dt.month_name()
-df['Weekday'] = df['Date'].dt.day_name()
-
-print("\n==Smart Datetime Feature Extract Karne ke Baad==")
-print(df)
+print(avg_salary_dept.sort_values(ascending=False))
