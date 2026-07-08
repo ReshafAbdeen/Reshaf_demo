03e74e4e0878a161623ac40d2d4f57baae686b1c
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-07-08 17:06:07 +0530

    Valorant VCT Pro-Player Analytics Pipeline

--- pandas_2.py
+++ pandas_2.py
@@ -1,40 +1,34 @@
-import pandas as pd
+# Valorant VCT Pro-Player Analytics Pipeline
+
+import pandas as pd 
 import numpy as np
 
-# 1. Generate realistic time-series data (100 days of sales)
-np.random.seed(42)
-dates = pd.date_range(start='2023-01-01', periods=100, freq='D')
-sales_data = {
-    'Date': dates,
-    'Store_A': np.random.normal(500, 50, 100),
-    'Store_B': np.random.normal(450, 60, 100)
+print("\033[1m Valorant Esport: Advance AI Data Pipeline \033[0m\n")
+
+vct_data = {
+    'Match_Date': ['12-May-2026', '13-May-2026', '12-May-2026', '12-May-2026', '13-May-2026', '14-May-2026'],
+    'Player': ['Tenz', 'Tenz', 'Tenz', "Forsaken", 'Forsaken', 'Forsaken'],
+    "Kills": [25, 12, 30, 18, 22, 15],
+    "Deaths": [10, 15, 12, 18, 14, 20],
+    "Ping_ms": [20, 150, 22, 25, 24, 300]
 }
-df = pd.DataFrame(sales_data)
 
-# 2. Add some artificial missing values and an outlier
-df.loc[10:14, 'Store_A'] = np.nan
-df.loc[85, 'Store_B'] = 2000.0
+df = pd.DataFrame(vct_data)
+print("--Raw Server Data--")
+print(df.head())
 
-# 3. Clean the data (Interpolate missing, cap outliers)
-df['Store_A'] = df['Store_A'].interpolate(method='linear')
-q_high = df['Store_B'].quantile(0.95)
-df['Store_B'] = np.where(df['Store_B'] > q_high, q_high, df['Store_B'])
+df["Match_Date"] = pd.to_datetime(df["Match_Date"])
 
-# 4. Feature Engineering: Extract Day of Week
-df['DayOfWeek'] = df['Date'].dt.day_name()
-df['Is_Weekend'] = df['Date'].dt.dayofweek.isin([5, 6])
+df["KD_Ratio"] = round(df["Kills"] / df["Deaths"], 2)
 
-# 5. Calculate 7-day rolling averages for trend analysis
-df['Store_A_7d_avg'] = df['Store_A'].rolling(window=7).mean()
-df['Store_B_7d_avg'] = df['Store_B'].rolling(window=7).mean()
+df["Performance"] = np.where(df["KD_Ratio"] > 1.5 , "MVP Level", "Normal")
 
-# 6. Calculate day-over-day percentage growth for Store A
-df['Store_A_Pct_Growth'] = df['Store_A'].pct_change() * 100
+ping_bins = [0, 50, 150, 500 ]
+ping_labels = ["Good", "Playable", "Extreme lag!"]
+df["Srever_Health"] = pd.cut(df["Ping_ms"] , bins=ping_bins, labels=ping_labels)
 
-# 7. Aggregate data: Average sales by Day of Week
-weekly_seasonality = df.groupby('DayOfWeek')[['Store_A', 'Store_B']].mean()
-best_days = weekly_seasonality.sort_values(by='Store_A', ascending=False)
+df = df.sort_values(by=['Player', 'Match_Date'])
+df["2-Match AVG Pings"] = df.groupby("Player")["Ping_ms"].transform(lambda x: x.rolling(2, min_periods=1).mean())
 
-# 8. Print the aggregated results and the final tail of the dataset
-print(best_days.head(3))
-print(df[['Date', 'Store_A', 'Store_A_7d_avg']].tail())
+print("===Finala AI-Ready Pipeline Ready hai===")
+print(df)
