3fc7dc22c2a95029fc03ecf79a0fbf4f2cefab01
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-07-08 16:35:20 +0530

    3D Arrays & Broadcasting (The Image Filter)

--- ddd.py
+++ ddd.py
@@ -1,31 +1,19 @@
-#Password Generator & Validator
+#3D Arrays & Broadcasting (The Image Filter)
 
-import random
-import string
-import re
+import numpy as np 
+print("\033[2m === AI Computer Vision: Numpy Image Filter === \033[0m\n")
 
-def generate_password(length=12):
-    chars = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(chars) for _ in range(length))
+image_matrix = np.array([
+    [[255, 0, 0], [0, 255, 0]],
+    [[0,  0, 255], [255, 255, 0]]
+])
 
-def validate_password(password):
-    if len(password) < 8:
-        return False, "Password too short."
-    if not re.search(r"[a-z]", password):
-        return False, "Needs lowercase letter."
-    if not re.search(r"[A-Z]", password):
-        return False, "Needs uppercase letter."
-    if not re.search(r"[0-9]", password):
-        return False, "Needs number."
-    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
-        return False, "Needs special character."
-    return True, "Strong password!"
+print("---Original 3D Matrix---")
+print(image_matrix)
+print(f"Image shape : {image_matrix.shape}")
 
-if __name__ == "__main__":
-    print("--- Password Tool ---")
-    new_pass = generate_password(14)
-    print(f"Generated Password: {new_pass}")
-    
-    test_pass = input("Enter a password to validate: ")
-    is_valid, message = validate_password(test_pass)
-    print(f"Result: {message}")
+red_filter = np.array([1, 0, 0])
+
+filtered_image = image_matrix * red_filter
+print("\n-- Iamge After 'Red Filter' Applied")
+print(filtered_image)
