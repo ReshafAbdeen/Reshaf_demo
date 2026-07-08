3bb2744e3726ef2e46e415a9162feb851b8e3a0d
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-07-08 16:12:23 +0530

    Python Loop vs NumPy Vectorization

--- cutie.py
+++ cutie.py
@@ -1,33 +1,28 @@
-#Command-Line To-Do List
+#Python Loop vs NumPy Vectorization
 
-def display_tasks(tasks):
-    print("\n--- To-Do List ---")
-    if not tasks:
-        print("List is currently empty.")
-    for i, t in enumerate(tasks, 1):
-        status = "[x]" if t['done'] else "[ ]"
-        print(f"{i}. {status} {t['name']}")
+import numpy as np
+import time
 
-def main():
-    tasks = []
-    while True:
-        display_tasks(tasks)
-        print("\n1. Add Task  2. Mark Done  3. Quit")
-        choice = input("Choice: ")
-        
-        if choice == '1':
-            tasks.append({"name": input("Task: "), "done": False})
-        elif choice == '2':
-            try:
-                idx = int(input("Task Number: ")) - 1
-                tasks[idx]['done'] = True
-            except (ValueError, IndexError):
-                print("Invalid task number.")
-        elif choice == '3':
-            print("Exiting...")
-            break
-        else:
-            print("Invalid input.")
+print("\033[1m"+"==Welcome to Numpy Vs Python Loops=="+"\033[0m")
 
-if __name__ == "__main__":
-    main()
+size = 1000000
+python_loop = list(range(size))
+numpy = np.arange(size)
+print(f"Loading {size} records in memory")
+
+print("\n======Python Loop Test=====")
+start_time_py = time.time()
+python_result = [x * 5 for x in python_loop]
+end_time_py = time.time()
+py_duration = end_time_py - start_time_py
+print(f"Numpy Time Duration {py_duration:.4f} second")
+
+print("\n======Numpy Test======")
+start_time_np = time.time()
+numpy_result = numpy * 5
+end_time_np = time.time()
+numpy_duration = end_time_np - start_time_np
+print(f"Numpy Time Duration {numpy_duration:.4f} second")
+
+speed_duration = py_duration / numpy_duration
+print(f"\n\033[1m Result :Numpy is {speed_duration}x Faster!\033[0m")
