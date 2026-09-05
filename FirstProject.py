7351936d3fd38f748fb59e1fd4cbc5b2dbafe3de
Zaynul Abdeen <abdeenzainul025@gmail.com>
2026-09-05 23:44:57 +0530

    Practice

--- FirstProject.py
+++ FirstProject.py
@@ -1,36 +1,39 @@
-# Algorithmic Trading Volatility Tracker (The Finance Tech)
+import datetime
 
-import pandas as pd
-import yfinance as yf
-import matplotlib.pyplot as plt
-from  matplotlib.animation import FuncAnimation
-print("\033[1m" + "==Algorithmic Trading: Live Crypto Data Fetcher=="+ "\033[0m\n")
 
-ticker = "BTC-USD"
+class LogSystem:
 
-print(f"Srever Se {ticker} Ka Live data pull kiya ja rha hai....Please Wait..")
+    def __init__(self, filename="app.log"):
+        self.filename = filename
 
-btc_data = yf.download(ticker, period="6mo")
+    def _write_log(self, level, message):
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        entry = f"[{timestamp}] [{level.upper()}] {message}"
+        print(entry)
+        with open(self.filename, "a") as f:
+            f.write(entry + "\n")
 
-df = pd.DataFrame(btc_data)
-print("=======Raw Market Data (First 5 Days)=======")
-print(df.head())
-df['SMA_20'] = df['Close'].rolling(window=20).mean()
+    def info(self, message):
+        self._write_log("INFO", message)
 
-print("\n===Data Info===")
-print(f"Total Raw & Columns {df.shape}")
+    def warning(self, message):
+        self._write_log("WARN", message)
 
-plt.style.use('dark_background')
-plt.figure(figsize=(10, 5))
+    def error(self, message):
+        self._write_log("ERROR", message)
 
-plt.plot(df.index, df['Close'], color='orange', linewidth=2, label='Bitcoin (BTC) Price')
-plt.plot(df.index, df['SMA_20'], color='cyan', linewidth=2, label='20 Days Trend (SMA)')
+    def read_logs((self)):
+        try:
+            with open(self.filename, "r") as f:
+                return f.readlines()
+        except FileNotFoundError:
+            return []
 
-plt.title("Bitcoin Trend - Last 6 Month", fontsize=14, fontweight='bold')
-plt.xlabel("Date", fontsize=14)
-plt.ylabel("Price in USD ($)", fontsize=14, color='lightgrey')
 
-plt.grid(True, linestyle=':', alpha=0.5, color='gray')
-plt.legend()
+logger = LogSystem()
+logger.info("Application started successfully.")
+logger.warning("High memory usage detected!")
+logger.error("Failed to connect to database.")
 
-plt.show()
+print("\n--- Reading Log File ---")
+print("".join(logger.read_logs()[-3:]))
